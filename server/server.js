fb14abb0613a0ec8a8e5e3f5f36d73f5d2f6db48
Shivam Sharma <jtrshivamsharma@gmail.com>
2026-03-24 01:54:13 +0530

    more changes

--- server/server.js
+++ server/server.js
@@ -13,13 +13,19 @@ const PORT = process.env.PORT || 3000;
 await connecDB()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://resume-builder-git-main-shivam4501s-projects.vercel.app',
+        'https://resume-builder-2enlmq7my-shivam4501s-projects.vercel.app'
+    ],
+    credentials: true
+}))
 
 app.get('/', (req,res)=>res.send("Server is live..."))
 app.use('/api/users', userRouter)
 app.use('/api/resumes', resumeRouter)
 app.use('/api/ai', aiRouter)
-
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
